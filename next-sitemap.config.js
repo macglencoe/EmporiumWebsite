1df3df4b4b95633fb3727c23b78596ffc9dda47c
macglencoe <101204247+macglencoe@users.noreply.github.com>
2025-03-01 12:51:10 -0500

    Excluded unused pages from sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,13 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://kingstreetemporium.com',
     generateRobotsTxt: true,
+    exclude: [
+        '/caffeine/roasts',
+        '/caffeine/origins',
+        '/pipes/brands',
+        '/pipes/materials',
+        '/pipes/types',
+        '/pipes/[slug]',
+        '/cigars/[slug]/size-real'
+    ]
 }
