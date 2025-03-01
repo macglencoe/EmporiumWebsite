64aed332e8fc44401af6baa9ef885d4f1492b217
macglencoe <101204247+macglencoe@users.noreply.github.com>
2025-03-01 14:40:34 -0500

    Revert another attempt at analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Analytics } from "@vercel/analytics/react"
 class CustomDocument extends Document {
   render() {
     return (
