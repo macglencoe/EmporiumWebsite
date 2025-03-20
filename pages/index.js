be054b0ed77f89b1f5eb46c3a85f91b5a43ec2d4
macglencoe <101204247+macglencoe@users.noreply.github.com>
2025-03-19 23:01:41 -0400

    resolves #40

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,8 @@ const Catalog = (props) => {
     }
   }, []);
 
+  const adminConsoleVersion = process.env.ADMIN_CONSOLE_VERSION ?? "1.0";
+
   return (
     <>
       <div id="fb-root"></div>
@@ -38,6 +40,84 @@ const Catalog = (props) => {
       <Layout>
         <div className="mobile-home-contact">
           <Contact ></Contact>
+        </div>
+        <div className='admin-console-dashboard'>
+          <header>
+            <h1>Admin Console</h1>
+            <b>Dashboard</b>
+            <p>Version: <b>{adminConsoleVersion}</b></p>
+            {console.log(process.env)}
+          </header>
+          <div className='vercel-container'>
+            
+            <h2>Hosting Dashboard</h2>
+            <p>This website is hosted with <b>Vercel</b>.<br></br> Click here to manage the site, view analytics, and more:</p>
+            <button onClick={() => window.open('https://vercel.com/king-street-emporium/emporium-website/deployments', '_blank')}><img src='https://upload.wikimedia.org/wikipedia/commons/5/5e/Vercel_logo_black.svg' alt='Vercel'></img></button>
+          </div>
+          <details className='accordion'>
+            <summary>How to use</summary>
+            <div>
+              <p>The admin console is a work in progress. As such, right now it only supports adding, editing, and deleting cigars.</p>
+              <ul>
+                <li>
+                  <h2>Adding a cigar</h2>
+                  <ol>
+                    <li>Click "Add New Cigar" anytime on the top-right of the page.</li>
+                    <li>Fill out the form. There are 2 required fields: Cigar Name and Brand</li>
+                    <li>Click "Submit" at the bottom of the form</li>
+                  </ol>
+                </li>
+                <li>
+                  <h2>Editing a cigar</h2>
+                  <ol>
+                    <li>In the cigar catalog, search for the cigar you want to edit</li>
+                    <li>Click on the cigar. <br></br>
+                      If this is a cigar that you added in this session, it will take you directly to the edit form. In this case, skip step 3</li>
+                    <li>Click "Edit" in the top-right</li>
+                    <li>Fill out the form</li>
+                    <li>Click "Submit" at the bottom of the form</li>
+                  </ol>
+                </li>
+                <li>
+                  <h2>Deleting a cigar</h2>
+                  <ol>
+                    <li>In the cigar catalog, search for the cigar you want to delete</li>
+                    <li>Click on the cigar.</li>
+                    <li>Click "Delete" in the top-right</li>
+                    <li>Read the confirmation page. If you are sure you want to delete the data, click "Delete Cigar" at the bottom.</li>
+                  </ol>
+                </li>
+                <li>
+                  <h2>Submitting your changes</h2>
+                  <p>Your changes are not immediately sent to the website. They are cached on your browser until you are ready to submit them all together.</p>
+                  <ol>
+                    <li>Click "Submit all Changes" at the top of the page, next to the "Add New Cigar" button</li>
+                    <li>Read the confirmation page carefully. If there looks to be anything out of place, amend them or contact the developer.</li>
+                    <li>If you are satisfied with the changes, click "Commit" at the bottom of the page.</li>
+                  </ol>
+                </li>
+              </ul>
+            </div>
+          </details>
+          <details>
+            <summary>Current Features ({adminConsoleVersion})</summary>
+            <div>
+              <ul>
+                <li>Vercel Authentication <a href='https://github.com/macglencoe/EmporiumWebsite/issues/47'>Issue #47</a></li>
+                <li>CRUD (Create, Read, Update, Delete) Form <a href='https://github.com/macglencoe/EmporiumWebsite/issues/29'>Issue #29</a></li>
+                <li>Real-time Validation <a href='https://github.com/macglencoe/EmporiumWebsite/issues/30'>Issue #30</a></li>
+                <li>Load Original Data <a href='https://github.com/macglencoe/EmporiumWebsite/issues/32'>Issue #32</a></li>
+                <li>Edit Cigar <a href='https://github.com/macglencoe/EmporiumWebsite/issues/28'>Issue #28</a></li>
+                <li>Delete Cigar <a href='https://github.com/macglencoe/EmporiumWebsite/issues/53'>Issue #53</a></li>
+                <li>Add Cigar <a href='https://github.com/macglencoe/EmporiumWebsite/issues/51'>Issue #51</a></li>
+                <li>Delete New Cigar <a href='https://github.com/macglencoe/EmporiumWebsite/issues/54'>Issue #54</a></li>
+                <li>Slug Generation & Validation <a href='https://github.com/macglencoe/EmporiumWebsite/issues/34'>Issue #34</a></li>
+                <li>Submit All Changes <a>Pending Issue</a></li>
+                <li>Link to Vercel Dashboard <a href='https://github.com/macglencoe/EmporiumWebsite/issues/40'>Issue #40</a></li>
+              </ul>
+              <a href='https://github.com/users/macglencoe/projects/2/views/3'>Check out the Roadmap!</a>
+            </div>
+          </details>
         </div>
 
         <div className='welcome-container-b' >
@@ -51,9 +131,9 @@ const Catalog = (props) => {
               <p>
                 Whether you're looking for fine cigars, pipes, a nice cup of coffee, or some good conversation, <b>we're here to share our expertise</b>!
               </p>
-              
 
-              
+
+
             </div>
             <div className="divider"></div>
             <div style={{
@@ -171,7 +251,7 @@ const Catalog = (props) => {
           <h1 id='podcast'>The Stick Figures</h1>
           <b>Our very own podcast</b>
           <p>What does a former 1%er biker, a modern farmer/ tobacconist, redneck battle rapper, and a submarine vet have in common? Nothing except that they are all brothers of the leaf. Tune in with the Stick Figures to hear about cigars, tobacco and life on this comedy cigar podcast.</p>
-          <iframe  src="https://open.spotify.com/embed/show/0xpAdXeTXnnh30J0HEVmoz?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+          <iframe src="https://open.spotify.com/embed/show/0xpAdXeTXnnh30J0HEVmoz?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 
           <div className='podcast-links'>
             <b>Find us on:</b>
@@ -183,7 +263,7 @@ const Catalog = (props) => {
               </li>
               <li>
                 <Link href={'https://jacobjpyle.podbean.com/'}><a>
-                <img src="//pbcdn1.podbean.com/fs1/site/images/badges/b600_1.png" alt="App download" data-categories="essential"></img>
+                  <img src="//pbcdn1.podbean.com/fs1/site/images/badges/b600_1.png" alt="App download" data-categories="essential"></img>
                 </a></Link>
               </li>
               <li>
@@ -197,7 +277,7 @@ const Catalog = (props) => {
                 </a></Link>
               </li>
             </ul>
-            
+
           </div>
         </section>
 
@@ -229,7 +309,7 @@ const Catalog = (props) => {
           }}>
             <div className='staff-container' >
               <div className='staff-image'>
-                <img alt="Edward Trout"src='/edtrout.jpg' />
+                <img alt="Edward Trout" src='/edtrout.jpg' />
               </div>
               <div className='staff-info'>
                 <h2>Edward Trout</h2>
@@ -246,12 +326,128 @@ const Catalog = (props) => {
               </div>
             </div>
           </div>
-          </div>
+        </div>
 
       </Layout>
 
       <style jsx>
         {`
+
+        div.admin-console-dashboard {
+          display: flex;
+          flex-direction: column;
+          align-items: flex-start;
+          gap: 1em;
+        }
+
+        div.admin-console-dashboard details {
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          background-color: var(--dl-color-theme-primary2);
+          border-radius: 10px;
+        }
+
+        div.admin-console-dashboard details summary {
+          cursor: pointer;
+          gap: 1em;
+          color: var(--dl-color-theme-secondary2);
+          font-weight: bold;
+          font-size: 1.3em;
+          padding: 0.5em;
+        }
+        div.admin-console-dashboard details > div {
+          display: flex;
+          flex-direction: column;
+          gap: 1em;
+          padding: 0.5em;
+        }
+        div.admin-console-dashboard details ul {
+          padding-left: 1em;
+          font-family: Inter;
+          display: flex;
+          flex-direction: column;
+          gap: 1em;
+        }
+        
+        div.admin-console-dashboard details ul ol {
+          display: flex;
+          flex-direction: column;
+          gap: 0.3em;
+        }
+
+        div.admin-console-dashboard details a {
+          color: var(--dl-color-theme-primary1);
+          font-weight: bold;
+        }
+
+        div.admin-console-dashboard details ul a::before {
+          content: '( ';
+          font-weight: normal;
+          color: var(--dl-color-theme-secondary1);
+        }
+
+        div.admin-console-dashboard details ul a::after {
+          content: ' )';
+          font-weight: normal;
+          color: var(--dl-color-theme-secondary1);
+        }
+
+        div.vercel-container {
+          display: flex;
+          flex-direction: column;
+          background-color: var(--dl-color-theme-primary2);
+          padding: 0.5em;
+          gap: 1em;
+          border-radius: 10px;
+        }
+
+        div.vercel-container h2 {
+          font-size: 1.5em;
+          color: var(--dl-color-theme-secondary2);
+        }
+
+        div.vercel-container button {
+          cursor: pointer;
+          padding: 0.7em;
+          border-radius: 10px;
+          border: none;
+          color: var(--dl-color-theme-primary1);
+          font-weight: bold;
+          width: min-content;
+          display: inline-block;
+          align-self: center;
+          background-position: 0px 0px;
+          background-size: 100% 100%;
+          transition: all 0.3s ease-out;
+          position: relative;
+          overflow: hidden;
+        }
+        div.vercel-container button img {
+          height: 2em;
+          z-index: 1;
+          position: relative;
+        }
+
+        div.vercel-container button::before {
+          content: '';
+          position: absolute;
+          width: 200%;
+          height: 400%;
+          top: -50%;
+          left: -50%;
+          z-index: 0;
+          background-image: radial-gradient(circle at left, var(--dl-color-theme-primary1) -190%, var(--dl-color-theme-primary2) 100%);
+          transition: all 1s ease-out;
+        }
+
+        div.vercel-container button:hover::before {
+          transform: rotate(-180deg);
+          background-position-x: 300px;
+        }
+
+
+
 
         section.podcast {
           background-color: var(--dl-color-theme-primary2);
