146e59c3fbac7afc867e18fbf46f1ddcdbbd4c40
macglencoe <101204247+macglencoe@users.noreply.github.com>
2025-04-26 21:21:58 -0400

    Revert "Update submit.js"
    This reverts commit 57b385e2eb84ddc9d4249d22c7628c9030e62e38.

--- pages/submit.js
+++ pages/submit.js
@@ -217,7 +217,7 @@ export const SubmitPage = (props) => {
                             </td>
                             <td className='equivalence'>
                                 <div>
-                                    {recentCommitSha == process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA && recentCommitSha == recentDataCommitSha &&
+                                    {recentCommitSha == process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA &&
                                     <p>=</p>}
                                     {recentCommitSha != process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA &&
                                         recentCommitSha == recentDataCommitSha && 
@@ -234,12 +234,8 @@ export const SubmitPage = (props) => {
                             <td className='equivalence'>
                                 <div>
                                     {currentCommitSha == process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA && <p>=</p>}
-                                    {currentCommitSha !== process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA && currentCommitSha == recentDataCommitSha &&
+                                    {currentCommitSha !== process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA && 
                                         <svg className='clickable' onClick={() => window.location.reload()} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-160q-134 0-227-93t-93-227q0-134 93-227t227-93q69 0 132 28.5T720-690v-110h80v280H520v-80h168q-32-56-87.5-88T480-720q-100 0-170 70t-70 170q0 100 70 170t170 70q77 0 139-44t87-116h84q-28 106-114 173t-196 67Z"/></svg>
-                                    }
-                                    {
-                                        currentCommitSha !== process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA && currentCommitSha !== recentDataCommitSha &&
-                                        <p>&lt;</p>
                                     }
                                 </div>
                             </td>
