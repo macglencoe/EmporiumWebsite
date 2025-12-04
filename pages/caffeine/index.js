acaffe5c193630d76de34815f8977a6da99607d6
Liam - Latitude <southernscotts112@gmail.com>
2025-12-03 19:48:58 -0500

    fix: z-index on hero

--- pages/caffeine/index.js
+++ pages/caffeine/index.js
@@ -53,10 +53,10 @@ function CaffeineHero({ count }) {
     return (
         <div className='border-9 border-double border-primary1/30 bg-secondary2 relative text-primary2 py-5 m-3'>
             {/* Backdrop */}
-            <div className='absolute inset-0 z-10 opacity-30'>
+            <div className='absolute inset-0 z-[0] opacity-30'>
                 <Image layout='fill' className="object-cover" src="https://images.unsplash.com/photo-1433891248364-3ce993ff0e92?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
             </div>
-            <div className='max-w-5xl mx-auto flex flex-row flex-wrap gap-5 z-20 relative'>
+            <div className='max-w-5xl mx-auto flex flex-row flex-wrap gap-5 z-[2] relative'>
                 {/* Left column */}
                 <div className='flex-2'>
                     <p className='text-lg'>Browse our selection of coffee and tea at The King Street Emporium, available to be enjoyed fresh in-store, or purchased by weight</p>
